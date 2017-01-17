b3b27167c099d9182528d957c048ac58ebe9c5e5
colin <acolin@incaztech.com>
2017-01-17 15:08:19 -0600

    next payments constructor tests passing

--- test/index.js
+++ test/index.js
@@ -1,9 +1,21 @@
 'use strict';
 
-var assert = require('assert');
+var ConektaCard = require('../lib/conektaCard');
+var NextPayments = require('../lib');
+var expect = require('chai').expect;
 
 describe('_initProcessor', function () {
   it('throws an exception when the processor type is invalid', function () {
-    assert(false, 'fuck you chuy');
+    var fn = function () {
+      return new NextPayments('');
+    };
+
+    expect(fn).to.throw(Error);
+  });
+
+  it('returns conekta processor class', function () {
+    var sub = new NextPayments('ConektaCard');
+
+    expect(sub.processor).to.be.an.instanceof(ConektaCard);
   });
 });
