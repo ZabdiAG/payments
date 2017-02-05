81d6e1e239cba3afd6211c8640f756629f817112
colin <acolin@incaztech.com>
2017-02-05 13:25:39 -0600

    standard eslint compliant

--- test/conektaCard/response.js
+++ test/conektaCard/response.js
@@ -1,27 +1,27 @@
-import Response from '../../lib/conektaCard/response';
-import {expect} from 'chai';
+import Response from '../../lib/conektaCard/response'
+import {expect} from 'chai'
 
 describe('Response', () => {
   it('returns null errors if none sent', () => {
-    let res = new Response();
+    let res = new Response()
 
-    expect(res.errors).to.equal(null);
-  });
+    expect(res.errors).to.equal(null)
+  })
 
   it('default error when no Joi obj error', () => {
-    let res = new Response({errors: {}});
+    let res = new Response({errors: {}})
 
     expect(res.errors).to.deep.equal({
       name: 'ProviderError',
       message: 'Hubo un error procesando la tarjeta'
-    });
-  });
+    })
+  })
 
   it('builds loopbackify errors when Joi obj', () => {
     const err = {
       isJoi: true,
       details: [{path: 'email', message: 'invalid email'}]
-    };
+    }
     const expected = {
       name: 'ValidationError',
       message: 'Hubo errores de validación',
@@ -29,9 +29,9 @@ describe('Response', () => {
         context: 'ConektaCard',
         messages: [{email: ['invalid email']}]
       }
-    };
-    let res = new Response({errors: err});
+    }
+    let res = new Response({errors: err})
 
-    expect(res.errors).to.deep.equal(expected);
-  });
-});
+    expect(res.errors).to.deep.equal(expected)
+  })
+})
